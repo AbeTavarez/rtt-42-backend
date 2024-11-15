31b423beea14cad3679a64ff6193b43abb6ad3ba
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-11-15 14:02:08 -0500

    routing

--- routes/chats.js
+++ routes/chats.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { fetchAllChats } from "../controllers/chats.js";
+import Chats from "../models/chats.js";
 
 const router = express.Router();
 
@@ -7,6 +7,43 @@ const router = express.Router();
  * GET /api/chats
  * @description Return all chats
  */
-router.get("/", fetchAllChats);
+router.get("/", async (req, res, next) => {
+  try {
+    const chats = await Chats.find();
+    res.json(chats);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+
+/**
+ * GET /api/:id
+ * @description return a single chat by the id
+ */
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const chat = await Chats.findById({ id });
+    res.json({ chat });
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+/**
+ * POST /api/chats/
+ * @description create a new chat
+ */
+router.post('/', async (req, res, next) => {
+    try {
+        const newChat = await new Chats({title: "New Chat"});
+        await newChat.save();
+        res.status(201).json({newChat});
+    } catch (e) {
+        console.error(e);
+        
+    }
+});
 
 export default router;
