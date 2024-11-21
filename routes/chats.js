50df954dd40ecd19e90286a015697b1e942a2855
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-11-21 11:15:55 -0500

    latest

--- routes/chats.js
+++ routes/chats.js
@@ -39,11 +39,48 @@ router.post('/', async (req, res, next) => {
     try {
         const newChat = await new Chats({title: "New Chat"});
         await newChat.save();
-        res.status(201).json({newChat});
+        res.status(201).json(newChat);
     } catch (e) {
         console.error(e);
         
     }
 });
+
+/**
+ * POST /api/chats/:id/message
+ * @description adds a new message in a chat
+ */
+router.post('/:id/message', async (req, res, next) => {
+  try {
+    const {id} = req.params;
+
+    // find the chat by the id
+    const chat = await Chats.findById(id);
+    console.log(chat);
+
+    // add a new message to the messages array
+    chat.messages.push(req.body);
+
+    await chat.save()
+
+    res.status(201).json(chat)
+    
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+router.delete('/:id', async(req, res, next) => {
+  try {
+    const {id} = req.params;
+    const deletedChat = await Chats.findByIdAndDelete(id);
+    res.json(deletedChat)
+  } catch (e) {
+    console.error(e);
+    
+  }
+})
+
+
 
 export default router;
